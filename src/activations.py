b3967b6d131a54b6a0457f4eece05cb20854d7c1
Jasman20 <jasmankaurchhabra@gmail.com>
2026-06-17 14:17:25 +0530

    Implement ReLU  (#8)
    * Implement ReLU and its derivative functions
    
    * Refactor ReLU functions into ReLU class

---
+++ src/activations.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class ReLU:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x > 0)
+        return x * self.mask
+
+    def backward(self, grad):
+        return grad * self.mask
