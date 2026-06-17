8fb80f5cec591ed3741c4bd632c9a4d944743839
Jasman20 <jasmankaurchhabra@gmail.com>
2026-06-17 13:53:28 +0530

    Refactor ReLU functions into ReLU class

--- src/activations.py
+++ src/activations.py
@@ -1,7 +1,12 @@
 import numpy as np
 
-def relu(x):
-    return np.maximum(0, x)
+class ReLU:
+    def __init__(self):
+        self.mask = None
 
-def relu_derivative(x):
-    return (x > 0).astype(float)
+    def forward(self, x):
+        self.mask = (x > 0)
+        return x * self.mask
+
+    def backward(self, grad):
+        return grad * self.mask
